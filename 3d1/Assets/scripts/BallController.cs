e779474c16c94a22a5f79ad4dbe880900fcf9734
Rubén Cepedello <rubencepedelloroman@ied.edu>
2017-11-17 20:47:23 +0100

    camera rotation and ball movement improved but not fixed

--- 3d1/Assets/scripts/BallController.cs
+++ 3d1/Assets/scripts/BallController.cs
@@ -3,20 +3,38 @@ using System.Collections;
 
 public class BallController : MonoBehaviour {
 
-	private float speed;
+	public float speed;
 	private Rigidbody rb;
+	private int isJumping;
+	private int maxSaltos;
+	public GameObject camera;
 
 	// Use this for initialization
 	void Start () {
 		this.rb = GetComponent<Rigidbody>();
-		this.speed = 5;
+		isJumping = 0;
+		maxSaltos = 1;
 	}
 	
 
 	void FixedUpdate () {
+		//global gravity
+		Physics.gravity = new Vector3 (0.0f, -100.0f, 0.0f);
+		//this is movement
 		float horizontalAxis = Input.GetAxis("Horizontal");
 		float verticalAxis = Input.GetAxis("Vertical");
 		Vector3 movement = new Vector3(horizontalAxis, 0.0f,verticalAxis);
-		rb.AddForce(movement*speed);
+		rb.AddForce(movement * speed);
+		//rb.AddRelativeForce(Vector3.forward * speed);
+
+		//this is for jumps
+		if(Input.GetKeyDown("space") && maxSaltos > 0){
+			Vector3 jumpForce = new Vector3 (0.0f, 30.0f, 0.0f);
+			rb.AddForce(jumpForce, ForceMode.Impulse);
+			maxSaltos--;
+		}
+	}
+	void OnCollisionEnter(Collision c){
+		this.maxSaltos = 1;
 	}
 }
---
+++ 3d1/Assets/scripts/cameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+	public GameObject ball;
+	private Vector3 diff;
+
+	// Use this for initialization
+	void Start () {
+		//offset es la diferencia de posiciones entre mi Gameobject camara y el gameobject que sigue.
+		this.diff = transform.position - ball.transform.position;
+	}
+		
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		
+		//orbit around
+		if(Input.GetKeyDown("r") || Input.GetKey("r")){
+			transform.RotateAround (ball.transform.position, new Vector3(0.0f, -90.0f, 0.0f), 100 * Time.deltaTime);
+			this.diff = transform.position- ball.transform.position;;
+		}
+
+		transform.position = ball.transform.position + this.diff;
+	}
+
+
+
+}
